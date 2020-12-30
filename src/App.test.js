53848553c8721163c2dc7168036234d54c1e403c
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-12-30 20:01:07 -0300

    Add test to App

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 //Importação do componente a ser testado
 import App, { calcularNovoSaldo } from './App'
@@ -33,15 +33,20 @@ describe('Componente principal', () => {
 
             expect(novoSaldo).toBe(100)
         })
-        
-        it('saque, o valor do saldo diminue', () => {
-            const valores = {
-                transacao: 'saque',
-                valor: 50
-            }
-            const novoSaldo = calcularNovoSaldo(valores, 150)
 
-            expect(novoSaldo).toBe(100)
+        it('saque, a transação tem que ser realizada ', () => {
+            const { getByText, getByTestId, getByLabelText } = render(<App />)
+
+            const saldo = getByText('R$ 1000')
+            const transacao = getByLabelText('Saque')
+            const valor = getByTestId('valor')
+            const botaoTransacao = getByText('Realizar operação')
+            
+            fireEvent.click(transacao, { target: { value: 'Saque' } })
+            fireEvent.change(valor, { target: { value: '10' } })
+            fireEvent.click(botaoTransacao)
+
+            expect(saldo.textContent).toBe('R$ 990')
         })
     })
 })

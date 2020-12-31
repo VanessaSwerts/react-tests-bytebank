904c2448915439966b67047cab0898b313e18183
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-12-31 10:50:30 -0300

    Fix error on Travis

--- src/App.test.js
+++ src/App.test.js
@@ -23,31 +23,36 @@ describe('Componente principal', () => {
         })
     })
 
-    // describe('Quando realizar uma transação: ', () => {
-    //     it('saque, o valor do saldo diminue', () => {
-    //         const valores = {
-    //             transacao: 'saque',
-    //             valor: 50
-    //         }
-    //         const novoSaldo = calcularNovoSaldo(valores, 150)
+    describe('Quando realizar uma transação: ', () => {
+        
+        it('que é um saque, o valor vai diminuir', () => {
+            const valores = {
+              transacao: 'saque',
+              valor: 50
+            }
+      
+            const novoSaldo = calcularNovoSaldo(valores, 150)
+      
+            expect(novoSaldo).toBe(100);
+          })
+     
 
-    //         expect(novoSaldo).toBe(100)
-    //     })
+        it('que é um saque, a transação deve ser realizada', () => {
+            render(<App />);
 
-    //     it('saque, a transação tem que ser realizada ', () => {
-    //         const { getByText, getByTestId, getByLabelText } = render(<App />)
+            const saldo = screen.getByText('R$ 1000');
+            const transacao = screen.getByLabelText('Saque');
+            const valor = screen.getByTestId('valor');
+            const botaoTransacao = screen.getByText('Realizar operação');
 
-    //         const saldo = getByText('R$ 1000')
-    //         const transacao = getByLabelText('Saque')
-    //         const valor = getByTestId('valor')
-    //         const botaoTransacao = getByText('Realizar operação')
-            
-    //         fireEvent.click(transacao, { target: { value: 'Saque' } })
-    //         fireEvent.change(valor, { target: { value: '10' } })
-    //         fireEvent.click(botaoTransacao)
+            expect(saldo.textContent).toBe('R$ 1000')
 
-    //         expect(saldo.textContent).toBe('R$ 990')
-    //     })
-    // })
+            fireEvent.click(transacao, { target: { value: 'saque' } });
+            fireEvent.change(valor, { target: { value: 10 } });
+            fireEvent.click(botaoTransacao);
+
+            expect(saldo.textContent).toBe('R$ 990');
+        })
+    })
 })
 

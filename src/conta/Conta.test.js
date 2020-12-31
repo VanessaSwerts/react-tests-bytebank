1d13cc4314211adf500921a6ba71709fd2a230a7
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-12-31 11:55:41 -0300

    Add mock test to Conta

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import Conta from './Conta'
 
 describe('Componente de conta', () => {
 
-    it('Exibir saldo da conta com formnatação monetária', () => {
+    it('Exibir saldo da conta com formatação monetária', () => {
         render(<Conta saldo={1000} />)
 
         const saldo = screen.getByTestId('saldo-conta')
 
         expect(saldo.textContent).toBe('R$ 1000')
+    })
+
+    it('Chama a função realizar transação ao clicar no botão', () => {
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />)
+
+        fireEvent.click(screen.getByText('Realizar operação'))
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled()
     })
 })
